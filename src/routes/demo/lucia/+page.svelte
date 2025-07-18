420f6ac9d804e128de4520e09d3480885fbe4c0a
Daniel Kosgei <dannykosgei@gmail.com>
2025-07-18 12:52:38 +0300

    feat: added shadcn-svelte

--- src/routes/demo/lucia/+page.svelte
+++ src/routes/demo/lucia/+page.svelte
@@ -2,11 +2,13 @@
 	import { enhance } from '$app/forms';
 	import type { PageServerData } from './$types';
 
+	import { Button } from "$lib/components/ui/button/index.js";
+
 	let { data }: { data: PageServerData } = $props();
 </script>
 
 <h1>Hi, {data.user.username}!</h1>
 <p>Your user ID is {data.user.id}.</p>
 <form method="post" action="?/logout" use:enhance>
-	<button>Sign out</button>
+	<Button>Click me</Button>
 </form>
